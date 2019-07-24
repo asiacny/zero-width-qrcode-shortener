ccc3eb1d553efefa3f1b3ddac5141f6535823b39
asiacny <9075003+asiacny@users.noreply.github.com>
2019-07-25 07:25:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <meta http-equiv="Cache-Control" content="no-cache,no-store, must-revalidate">
 <meta http-equiv="Cache" content="no-cache">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes"/>
+<title>Url Shortener</title>
+<link rel="icon" href="data:image/ico;base64,aWNv">
 <script>
 function showUser(str) {
     if (str == "") {
